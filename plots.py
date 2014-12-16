9a665b05ff2f9ca2e8d033cec9dab13536a7f3fa
Greg Reda <gjreda+github@gmail.com>
2014-12-15 19:48:15 -0600

    add functions to get data into a format that will play nice with DataTables.js

--- plots.py
+++ plots.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import seaborn as sns
+import json
 sns.set(style='white', palette='muted')
 
 def ranker(df):
@@ -16,6 +17,69 @@ def bootstrap(data, statfunction=np.mean):
     np.random.seed(42)
     samples = pd.DataFrame(np.random.choice(data, size=(10000, len(data)), replace=True))
     return samples.apply(statfunction, axis=1)
+
+def run_player_bootstraps(df):
+    """
+    Runs the bootstrap function for every player in the given DataFrame.
+    Returns
+    -------
+    {
+        Peyton Manning: {
+            points: { mean: [], median: [] },
+            relative: { mean: [], median: [] }
+        }, 
+        Matt Forte: {
+            points: { mean: [], median: [] },
+            relative: { mean: [], median: [] }
+        } ...
+    }
+    """
+    players = {}
+    for player in df.name.unique().tolist():
+        print('Bootstrapping for {}'.format(player))
+        cond = df.name == player
+        players[player] = {
+            'points': {
+                'mean': bootstrap(df[cond].point_diff).tolist(),
+                'median': bootstrap(df[cond].point_diff, np.median).tolist()
+            },
+            'relative': {
+                'mean': bootstrap(df[cond].relative_diff).tolist(),
+                'median': bootstrap(df[cond].relative_diff, np.median).tolist()
+            }
+        }
+    return players
+
+def run_weekly_bootstraps(df):
+    """
+    Runs the bootstrap function for every week in the given DataFrame.
+    Returns
+    -------
+    {
+        1: {
+            points: { mean: [], median: [] },
+            relative: { mean: [], median: [] }
+        }, 
+        2: {
+            points: { mean: [], median: [] },
+            relative: { mean: [], median: [] }
+        } ...
+    }
+    """
+    weeks = {}
+    for i in range(1, df.week.max() + 1):
+        cond = df.week == i
+        weeks[i] = {
+            'points': {
+                'mean': bootstrap(df[cond].point_diff).tolist(),
+                'median': bootstrap(df[cond].point_diff, np.median).tolist()
+            },
+            'relative': {
+                'mean': bootstrap(df[cond].relative_diff).tolist(),
+                'median': bootstrap(df[cond].relative_diff, np.median).tolist()
+            }
+        }
+    return weeks
 
 def get_ci(data, alpha=0.05):
     """Return confidence intervals"""
@@ -62,8 +126,39 @@ def boxplots(data, filename, title='', xlabel='', ylabel='', xlim=(-5,5), order=
     b.spines['right'].set_visible(False)
     plt.savefig(filename)
 
-projections = pd.read_csv('data/projections.csv', index_col=['player_id', 'name', 'season', 'week'])
-scoring = pd.read_csv('data/scoring.csv', index_col=['player_id', 'name', 'season', 'week'])
+def get_datatables_input(df):
+    """Prepping data so we can write to a file and use in datatables.js."""
+    # This is horrible. I deserve it. It's my own fault.
+    data = []
+    players = df.name.unique().tolist()
+    for i, player in enumerate(players):
+        print('Running for {} ({} of {})'.format(player, i + 1, len(players)))
+        cond = df.name == player
+        pos = df[cond].position[0]
+        team = df[cond].team[ len(df[cond]) - 1 ] # most recent team
+        abs_sims = bootstrap(df[cond].point_diff)
+        rel_sims = bootstrap(df[cond].relative_diff)        
+        data.append({
+            'name': player,
+            'position': pos,
+            'team': team,
+            'avg_points': round(df[cond].total_pts.mean(), 3),
+            'total_points': df[cond].total_pts.sum(),
+            'abs_lower_95': round(np.percentile(abs_sims, 2.5), 3),
+            'abs_mean_error': round(np.mean(abs_sims), 3),
+            'abs_upper_95': round(np.percentile(abs_sims, 97.5), 3),
+            'rel_lower_95': round(np.percentile(rel_sims, 2.5), 3),
+            'rel_mean_error': round(np.mean(rel_sims), 3),
+            'rel_upper_95': round(np.percentile(rel_sims, 97.5), 3)
+        })
+    return data
+
+def write_datatables_input(data):
+    with open('visualization/data.json', 'w') as f:
+        json.dump({'data': data}, f, indent=2, separators=(',', ': '))
+
+projections = pd.read_csv('data/projections-espn.csv', index_col=['player_id', 'name', 'season', 'week'])
+scoring = pd.read_csv('data/scoring-espn.csv', index_col=['player_id', 'name', 'season', 'week'])
 
 espn = scoring.join(projections.query('projected_pts > 0').projected_pts, how='right')
 espn['point_diff'] = (espn.projected_pts - espn.total_pts)
@@ -86,18 +181,6 @@ for pos, n in relevant.iteritems():
     booted_positions['relative']['mean'][pos] = bootstrap(ranked[condition]['relative_diff'], statfunction=np.mean)
     booted_positions['relative']['median'][pos] = bootstrap(ranked[condition]['relative_diff'], statfunction=np.median)
 fantasy_relevant = pd.concat(frames)
-
-
-booted_weeks = {
-    'points': {'mean': {}, 'median': {}},
-    'relative': {'mean': {}, 'median': {}}
-}
-for i in range(1, espn.week.max() + 1):
-    condition = fantasy_relevant.week == i
-    booted_weeks['points']['mean'][i] = bootstrap(fantasy_relevant[condition]['point_diff'], statfunction=np.mean)
-    booted_weeks['points']['median'][i] = bootstrap(fantasy_relevant[condition]['point_diff'], statfunction=np.median)
-    booted_weeks['relative']['mean'][i] = bootstrap(fantasy_relevant[condition]['relative_diff'], statfunction=np.mean)
-    booted_weeks['relative']['median'][i] = bootstrap(fantasy_relevant[condition]['relative_diff'], statfunction=np.median)
 
 
 # plots, plots, plots, plots plots plots
