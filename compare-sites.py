a790554b19b7c36e685ffc7e5485b57e385a41ef
Greg Reda <gjreda+github@gmail.com>
2014-12-16 19:10:41 -0600

    add code for bootstrapping and histograms

--- compare-sites.py
+++ compare-sites.py
@@ -1,16 +1,13 @@
 """
-Compares prognosticators making fantasy football projections
+Compares prognosticators making fantasy football projections.
 """
-from utils import ranker, bootstrap, histogram, boxplots
 from mappings import ESPN_SCORING, FANTASY_RELEVANT, NAME_CORRECTIONS
+import utils
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
-
-def calculate_points(df):
-    """Vectorizing the fractional point calculation."""
-    pass
+import re
 
 def clean_name(player_name):
     """
@@ -23,6 +20,13 @@ def clean_name(player_name):
 
     name = ''.join(char for char in name if char not in '.,')
     return name.strip().lower()
+
+def get_team_name(player_name):
+    team = re.findall('\((.*)\)', player_name)
+    if team:
+        if team == 'WAS':
+            return 'Wsh'
+        return team[0].capitalize()
 
 def read_projections():
     """
@@ -43,7 +47,7 @@ def read_scoring():
     calculate actual points scored; use fractional to be more precise
     ESPN only uses whole points; takes a bit bc of the looping
     """
-    scoring = pd.read_csv('data/scoring.csv')
+    scoring = pd.read_csv('data/scoring-espn.csv')
     scoring['PTS_SCORED'] = 0
     for idx, col in scoring.iterrows():
         if scoring.ix[idx, 'position'] == 'K':
@@ -55,11 +59,11 @@ def read_scoring():
 def get_fantasy_relevant(projections_df):
     """
     get only fantasy relevant players; top N at each position
-    see mappings.FANTASY_RELEVANT
+    see mappings.FANTASY_RELEVANT; uses utils.ranker function
     """
-    cols_to_groupby = ['PLAYER_NAME', 'WEEK', 'EXPERT']
-    ranked = projections_df.groupby(cols_to_groupby).apply(ranker)
-    ranked.reset_index(inplace=True)
+    cols_to_groupby = ['EXPERT', 'POSITION', 'WEEK']
+    ranked = projections_df.groupby(cols_to_groupby).apply(utils.ranker)
+    # ranked.reset_index(inplace=True)
 
     cols = ['PLAYER_NAME', 'POSITION', 'WEEK', 'EXPERT', 'POSITION_RANK'
             'FPTS', 'PTS_SCORED', 'PTS_DIFF', 'REL_DIFF']
@@ -73,26 +77,95 @@ def get_fantasy_relevant(projections_df):
 def bootstrap_experts(df, col_to_bootstrap, statfunction=np.mean):
     """
     Runs the bootrapping method for each expert on the given column_name.
-    """
-    np.random.seed(42)
+    Uses bootstrapped mean by default.
     
+    Parameters
+    ----------
+    df: pandas.DataFrame
+    col_to_bootstrap: string
+        Column to apply bootstrapping technique to
+    statfunction: function
+        Statistical function to use for bootstrapping.
+
+    Returns
+    -------
+    """
+    np.random.seed(42) # set random seed for consistent results
     experts = {}
+    for expert_name in df.EXPERT.unique().tolist():
+        # filter to the relevant expert and column
+        data = df[ df.EXPERT == expert_name ][col_to_bootstrap]
+        # use the bootstrap to create 10000 x N matrix
+        sims = np.random.choice(data, size=(10000, len(data)), replace=True)
+        # apply statfunction to each row (simulation) of the matrix
+        booted_vals = pd.DataFrame(sims).apply(statfunction, axis=1).tolist()
+        experts[expert_name] = booted_vals
+    return experts
+
+def bootstrap_experts_positions(df, col_to_bootstrap, statfunction=np.mean):
+    np.random.seed(42) # set random seed for consistent results
+    results = {}
+    experts = df.EXPERT.unique().tolist()
+    positions = df.POSITION.unique().tolist()
+    for ex_name in experts:
+        pos_results = {}
+
+        for pos in positions:
+            # filter to the relevant expert and position
+            condition = (df.EXPERT == ex_name) & (df.POSITION == pos)
+            data = df[condition][col_to_bootstrap]
+            # use the bootstrap to create 10000 x N matrix
+            sims = np.random.choice(data, size=(10000, len(data)), replace=True)
+            # apply statfunction to each row (simulation) of the matrix
+            booted_vals = pd.DataFrame(sims).apply(statfunction, axis=1).tolist() 
+            pos_results[pos] = booted_vals
+
+        results[ex_name] = pos_results
+    return results
+
+def generate_histograms(data):
+    for expert, values in data.iteritems():
+        filename = ''.join(char for char in expert if char not in '.,')
+        filename = filename.strip().replace(' ', '-').lower()
+        utils.histogram(
+            data=values,
+            filename='charts/fantasypros/{}'.format(filename),
+            title='Mean Point Error - {}'.format(expert),
+            figsize=(10,5),
+            titlesize=26,
+            xsize=26,
+            xlim=(-2.75, 2.75),
+            small=True
+        )
+
+def generate_histogram_grid(data):
     pass
 
 if __name__ == '__main__':
     projections = read_projections()
     scoring = read_scoring()
+    fantasy_relevant = get_fantasy_relevant(projections)
 
     # clean up names for joining projections with actual scoring
-    projections['PLAYER_NAME'] = projections.Player.apply(clean_name)
+    fantasy_relevant['PLAYER_NAME'] = fantasy_relevant.Player.apply(clean_name)
+    fantasy_relevant['TEAM'] = fantasy_relevant.Player.apply(get_team_name)
     scoring['PLAYER_NAME'] = scoring.name.apply(clean_name)
-    joined = pd.merge(projections, scoring,
+
+    joined = pd.merge(fantasy_relevant, scoring,
                         left_on=['PLAYER_NAME', 'WEEK', 'POSITION'],
                         right_on=['PLAYER_NAME', 'week', 'position'],
                         how='left')
-
-    joined[joined.PTS_SCORED.isnull()].PLAYER_NAME.value_counts()
-    cols = ['PLAYER_NAME', 'team', 'POSITION', 'WEEK',
-            'EXPERT', 'FPTS', 'PTS_SCORED']
-    joined = joined[cols]
+    cols = ['PLAYER_NAME', 'TEAM', 'POSITION', 'WEEK', 'EXPERT',
+            'POSITION_RANK', 'FPTS', 'PTS_SCORED']
+    
+    # drop players that don't have teams - Brandon Jacobs, JP Wilson, etc.
+    joined.dropna(how='all', subset=['TEAM', 'PTS_SCORED'], inplace=True)
     joined['PTS_DIFF'] = (joined.FPTS - joined.PTS_SCORED)
+    joined['REL_DIFF'] = (joined.PTS_DIFF / joined.FPTS)
+
+    abs_err_by_expert = bootstrap_experts(joined, 'PTS_DIFF')
+    abs_err_by_expert_position = bootstrap_experts_positions(joined, 'PTS_DIFF')
+    rel_err_by_expert = bootstrap_experts(joined, 'REL_DIFF')
+    rel_err_by_expert_position = bootstrap_experts_positions(joined, 'REL_DIFF')
+
+    generate_histograms(abs_err_by_expert)
